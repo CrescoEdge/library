1a14403e53b0fbab5218343fb482a5ec2ddedf57
Cody Bumgardner <cody@uky.edu>
2019-04-05 15:12:31 -0400

    minor change on pNode

--- src/main/java/io/cresco/library/app/gEdge.java
+++ src/main/java/io/cresco/library/app/gEdge.java
@@ -9,12 +9,12 @@ public class gEdge {
 	  public String edge_id;
 	  public String node_from;
 	  public String node_to;
-	  public Map<String, String> params;
-	  public Map<String, String> params_to;
-	  public Map<String, String> params_from;
 
-	public gEdge(String edge_id, String node_from, String node_to)
-	  {
+	  private Map<String, String> params;
+	  private Map<String, String> params_to;
+	  private Map<String, String> params_from;
+
+	public gEdge(String edge_id, String node_from, String node_to) {
 		 this.edge_id = edge_id;
 		 this.node_from = node_from;
 		 this.node_to = node_to;
@@ -23,14 +23,34 @@ public class gEdge {
 		 params_from = new HashMap<>();
 	  }
 
-	public gEdge(String edge_id, String node_from, String node_to, Map<String, String> params)
-	{
+	public gEdge(String edge_id, String node_from, String node_to, Map<String, String> params) {
 		this.edge_id = edge_id;
 		this.node_from = node_from;
 		this.node_to = node_to;
 		this.params = params;
 		params_to = new HashMap<>();
 		params_from = new HashMap<>();
+	}
+
+	public Map<String,String> getParams() {
+		if(params == null) {
+			params = new HashMap<>();
+		}
+		return params;
+	}
+
+	public Map<String,String> getParamsTo() {
+		if(params_to == null) {
+			params_to = new HashMap<>();
+		}
+		return params_to;
+	}
+
+	public Map<String,String> getParamsFrom() {
+		if(params_from == null) {
+			params_from = new HashMap<>();
+		}
+		return params_from;
 	}
 
 
