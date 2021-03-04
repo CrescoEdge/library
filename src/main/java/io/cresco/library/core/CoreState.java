53a2e694ba2a4d45d898dab42917a6310a2d2636
Cody Bumgardner <cody@uky.edu>
2021-03-04 15:06:31 -0500

    in rare testing cases we might want to stop the controller

--- src/main/java/io/cresco/library/core/CoreState.java
+++ src/main/java/io/cresco/library/core/CoreState.java
@@ -4,6 +4,7 @@ package io.cresco.library.core;
 public interface CoreState {
 
 	public boolean updateController(String jarPath);
+	public boolean stopController();
 	public boolean restartController();
 	public boolean restartFramework();
 	public boolean killJVM();
